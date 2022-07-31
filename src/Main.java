a70aa0da36ba8d1dad4453db169d4cac2c2f0d01
allllla2000 <intergalacticwoman@gmail.com>
2022-07-31 20:56:30 +0300

    выполненное дз 1.7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задание 1
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println();
+        //задание 2
+        System.out.println("Задание 2");
+        System.out.println(fullName.toUpperCase());
+        System.out.println();
+        //задание 3
+        System.out.println("Задание 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace("ё", "е"));
+        System.out.println();
+
+
+
+
+
+
+
+
     }
 }
